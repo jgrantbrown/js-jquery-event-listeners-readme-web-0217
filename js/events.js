10c0df268b217d3fb36895413ea212cb5684b11d
JGBrown <j.grant.brown@gmail.com>
2017-06-06 13:24:33 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $('p:first').on('click', () => {
+    alert("I have been clicked!!!")
+  })
+}
+
+function frameIt() {
+  $('img:first').on('load', () => {
+    $('img:first').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', (key) => {
+    if (key.which === 71) {
+      alert('g was pressed!')
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
+  frameIt()
+  submitIt()
+  pressIt()
 
 });
